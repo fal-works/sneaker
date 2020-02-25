10f72daa873aada69b8a78a0769fc77d689a0cb4
FAL <falworks.contact@gmail.com>
2020-02-25 15:00:06 +0900

    add StringExtensin functions

--- src/sneaker/format/StringExtension.hx
+++ src/sneaker/format/StringExtension.hx
@@ -1,10 +1,20 @@
 package sneaker.format;
 
+import haxe.ds.Option;
+
 @:nullSafety(Strict)
 class StringExtension {
 	@:nullSafety(Off)
 	public static inline function formatNullable(s:Null<Dynamic>):String {
 		return cast s;
+	}
+
+	public static inline function toNullableString(s:Null<Dynamic>):Null<String> {
+		return s;
+	}
+
+	public static inline function toOptionalString(s:Null<Dynamic>):Option<String> {
+		return s != null ? Some(s) : None;
 	}
 
 	public static inline function sliceAfterLastDot(s:String):String {
