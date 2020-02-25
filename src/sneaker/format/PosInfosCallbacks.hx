1484915a27310902020fb6558dfc6bc654155467
FAL <falworks.contact@gmail.com>
2020-02-25 20:29:07 +0900

    format: add position format functions

---
+++ src/sneaker/format/PosInfosCallbacks.hx
@@ -0,0 +1,16 @@
+package sneaker.format;
+
+@:nullSafety(Strict)
+class PosInfosCallbacks {
+	public static final formatFile = PosInfosExtension.formatFile;
+	public static final formatClass = PosInfosExtension.formatClass;
+	public static final formatMethod = PosInfosExtension.formatMethod;
+	public static final formatClassWithoutModule = PosInfosExtension.formatClassWithoutModule;
+	public static final formatClassMethod = PosInfosExtension.formatClassMethod;
+	public static final formatClassMethodWithoutModule = PosInfosExtension.formatClassMethodWithoutModule;
+	public static final formatClassMethodLine = PosInfosExtension.formatClassMethodLine;
+	public static final formatClassMethodLineWithoutModule = PosInfosExtension.formatClassMethodLineWithoutModule;
+	public static final formatFileWithoutPath = PosInfosExtension.formatFileWithoutPath;
+	public static final formatFileLine = PosInfosExtension.formatFileLine;
+	public static final formatFileLineWithoutPath = PosInfosExtension.formatFileLineWithoutPath;
+}
--- src/sneaker/format/PosInfosExtension.hx
+++ src/sneaker/format/PosInfosExtension.hx
@@ -1,20 +1,57 @@
 package sneaker.format;
 
+using sneaker.format.StringExtension;
+
 import haxe.PosInfos;
 
 @:nullSafety(Strict)
 class PosInfosExtension {
 	static final noPositionInfo = "(No position info)";
 
-	public static function formatClassMethod(pos:Null<PosInfos>) {
+	/** Example: "mydirectory/MyFile.hx" */
+	public static function formatFile(?pos:Null<PosInfos>) {
+		return pos == null ? noPositionInfo : '${pos.fileName}';
+	}
+	/** Example: "my_module.MyClass" */
+	public static function formatClass(?pos:Null<PosInfos>) {
+		return pos == null ? noPositionInfo : '${pos.className}';
+	}
+	/** Example: "myMethod" */
+	public static function formatMethod(?pos:Null<PosInfos>) {
+		return pos == null ? noPositionInfo : '${pos.methodName}';
+	}
+
+	/** Example: "MyClass" */
+	public static function formatClassWithoutModule(?pos:Null<PosInfos>) {
+		return pos == null ? noPositionInfo : '${pos.className.sliceAfterLastDot()}';
+	}
+	/** Example: "my_module.MyClass::myMethod" */
+	public static function formatClassMethod(?pos:Null<PosInfos>) {
 		return pos == null ? noPositionInfo : '${pos.className}::${pos.methodName}';
 	}
-
-	public static function formatClassMethodLine(pos:Null<PosInfos>) {
+	/** Example: "MyClass::myMethod" */
+	public static function formatClassMethodWithoutModule(?pos:Null<PosInfos>) {
+		return pos == null ? noPositionInfo : '${pos.className.sliceAfterLastDot()}::${pos.methodName}';
+	}
+	/** Example: "my_module.MyClass::myMethod line 1" */
+	public static function formatClassMethodLine(?pos:Null<PosInfos>) {
 		return pos == null ? noPositionInfo : '${pos.className}::${pos.methodName} line ${pos.lineNumber}';
 	}
+	/** Example: "MyClass::myMethod line 1" */
+	public static function formatClassMethodLineWithoutModule(?pos:Null<PosInfos>) {
+		return pos == null ? noPositionInfo : '${pos.className.sliceAfterLastDot()}::${pos.methodName} line ${pos.lineNumber}';
+	}
 
-	public static function formatFileLine(pos:Null<PosInfos>) {
+	/** Example: "MyFile.hx" */
+	public static function formatFileWithoutPath(?pos:Null<PosInfos>) {
+		return pos == null ? noPositionInfo : '${pos.fileName.sliceAfterLastSlash()}';
+	}
+	/** Example: "mydirectory/MyFile.hx line 1" */
+	public static function formatFileLine(?pos:Null<PosInfos>) {
 		return pos == null ? noPositionInfo : '${pos.fileName} line ${pos.lineNumber}';
+	}
+	/** Example: "MyFile.hx line 1" */
+	public static function formatFileLineWithoutPath(?pos:Null<PosInfos>) {
+		return pos == null ? noPositionInfo : '${pos.fileName.sliceAfterLastSlash()} line ${pos.lineNumber}';
 	}
 }
