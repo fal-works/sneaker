b299e78146818b136c5970211ca241b4ca6b7a71
FAL <falworks.contact@gmail.com>
2020-02-25 21:16:42 +0900

    assertion: add test cases

--- test/sneaker/AssertionTest.hx
+++ test/sneaker/AssertionTest.hx
@@ -1,6 +1,7 @@
 package sneaker;
 
 import sneaker.assertion.Asserter.*;
+import sneaker.tag.Tag;
 import sneaker.log.Print.println;
 
 @:nullSafety(Strict)
@@ -16,7 +17,13 @@ class AssertionTest {
 		final thisIsGreater = 1;
 		assert(thisIsLess < thisIsGreater);
 	};
-	public static final assertTests = runCases.bind([assertTrue, assertFalse]);
+	public static final assertTagFalse = () -> {
+		describe("This raises an exception because the tag has wrong name.");
+		final tag = new Tag("someTagName");
+		tag.name = "someTagName";
+		assert(tag.name == "otherTagName", tag);
+	};
+	public static final assertTests = runCases.bind([assertTrue, assertFalse, assertTagFalse]);
 
 	// unwrap
 	public static final unwrapOk = () -> {
@@ -25,11 +32,17 @@ class AssertionTest {
 		println(unwrap(obj));
 	};
 	public static final unwrapError = () -> {
-		final obj:Null<Dynamic> = null;
 		describe('This raises an exception because (obj) is null.');
+		final obj:Null<Dynamic> = null;
 		println(unwrap(obj));
 	};
-	public static final unwrapTests = runCases.bind([unwrapOk, unwrapError]);
+	public static final unwrapTagError = () -> {
+		describe('This raises an exception with a tag name, because (obj) is null.');
+		final obj:Null<Dynamic> = null;
+		final relatedTag = new Tag("someTagName");
+		println(unwrap(obj, relatedTag));
+	};
+	public static final unwrapTests = runCases.bind([unwrapOk, unwrapError, unwrapTagError]);
 
 	// all
 	public static final all = runCases.bind([assertTests, unwrapTests]);
