b34fdb5dd5afdfae081187b0ba0d8192bce2b216
FAL <falworks.contact@gmail.com>
2020-02-25 21:29:06 +0900

    edit Main

--- test/Main.hx
+++ test/Main.hx
@@ -3,6 +3,9 @@ package;
 // dunno how to do tests!
 class Main {
 	static function main() {
-		runCases([sneaker.LogTest.all, sneaker.AssertionTest.all]);
+		runCases([
+			sneaker.LogTest.all,
+			sneaker.AssertionTest.all
+		]);
 	}
 }
