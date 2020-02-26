ca35a2f452b59ab74ec1e759490040cd2670394f
FAL <falworks.contact@gmail.com>
2020-02-26 23:37:53 +0900

    format: add Int function

--- src/sneaker/format/IntExtension.hx
+++ src/sneaker/format/IntExtension.hx
@@ -2,11 +2,19 @@ package sneaker.format;
 
 class IntExtension {
 	static final zero = "0";
+	static final space = " ";
 
 	/**
 	 * Casts `Int` to `String`, left-padding with zeros.
 	 */
 	public static function addLeadingZeros(value:Int, digitCount:Int):String {
 		return StringTools.lpad(Std.string(value), zero, digitCount);
+	}
+
+	/**
+	 * Casts `Int` to `String`, left-padding with spaces.
+	 */
+	 public static function addLeadingSpaces(value:Int, digitCount:Int):String {
+		return StringTools.lpad(Std.string(value), space, digitCount);
 	}
 }
