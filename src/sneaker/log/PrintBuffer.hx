d0ebe130ae2eb59acdb6601155baa6d35691739e
FAL <falworks.contact@gmail.com>
2020-02-26 19:04:33 +0900

    log: add PrintBuffer

---
+++ src/sneaker/log/PrintBuffer.hx
@@ -0,0 +1,67 @@
+package sneaker.log;
+
+import sneaker.format.StringBuffer;
+import sneaker.assertion.Asserter.unwrap;
+
+class PrintBuffer {
+	/**
+	 * The top element of the buffer stack.
+	 */
+	public static var current(get, never):StringBuffer;
+	static inline function get_current():StringBuffer {
+		return stack[topIndex];
+	}
+
+	/**
+	 * Pushes `buffer` to the buffer stack.
+	 */
+	public static function push(buffer:StringBuffer):Void {
+		stack.push(buffer);
+		++topIndex;
+	}
+
+	/**
+	 * Creates a new `StringBuffer` and pushes it to the buffer stack.
+	 * @return The newly created buffer.
+	 */
+	public static function pushNew() {
+		final newBuffer = new StringBuffer();
+		push(newBuffer);
+
+		return newBuffer;
+	}
+
+	/**
+	 * Pops the top element of the buffer stack.
+	 * If this operation empties the stack, a new buffer element is automatically added.
+	 */
+	public static function pop():StringBuffer {
+		final popped = unwrap(stack.pop());
+
+		if (topIndex > 0)
+			--topIndex;
+		else
+			stack.push(new StringBuffer());
+
+		return popped;
+	}
+
+	/**
+	 * Drops the current buffer by `pop()` and prints its content by `Print.printlnDirect()`.
+	 */
+	public static function flush():Void {
+		Print.printlnDirect(pop().toString());
+	}
+
+	/**
+	 * Clears the buffer stack.
+	 */
+	public static function clear():Void {
+		stack.resize(0);
+		stack.push(new StringBuffer());
+		topIndex = 0;
+	}
+
+	static final stack:Array<StringBuffer> = [new StringBuffer()];
+	static var topIndex = 0;
+}
