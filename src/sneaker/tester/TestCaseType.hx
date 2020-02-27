9a537cf083b1adfe92cb5e4aade644a00e40ea6c
FAL <falworks.contact@gmail.com>
2020-02-27 21:23:22 +0900

    edit comment

--- src/sneaker/tester/TestCaseType.hx
+++ src/sneaker/tester/TestCaseType.hx
@@ -4,12 +4,20 @@ package sneaker.tester;
  * Type of test cases, which indicates what kind of results is expected.
  */
 enum TestCaseType {
-	/** The test case should complete successfully. */
+	/**
+	 * The test case should complete successfully.
+	 */
 	Ok;
 
-	/** The test case should fail and throw anything. */
+	/**
+	 * The test case should fail and throw anything.
+	 *
+	 * Note that it will not be automatically checked what kind of exception.
+	 */
 	Fail;
 
-	/** The result cannot be judged automatically and needs to be confirmed visually. */
+	/**
+	 * The result cannot be judged automatically and needs to be confirmed visually.
+	 */
 	Visual;
 }
