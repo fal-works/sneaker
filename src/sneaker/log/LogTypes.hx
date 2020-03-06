ef5833905ba141fdf875e99829f2960e94b41a9c
FAL <falworks.contact@gmail.com>
2020-03-06 20:53:56 +0900

    log: refactor prefixes of LogTypes

--- src/sneaker/log/LogTypes.hx
+++ src/sneaker/log/LogTypes.hx
@@ -4,11 +4,19 @@ package sneaker.log;
 	Collection of standard log types.
 **/
 class LogTypes {
-	public static var fatal = new LogType("[FATAL] ");
-	public static var error = new LogType("[ERROR] ");
-	public static var warn = new LogType("[WARN]  ");
-	public static var info = new LogType("[INFO]  ");
-	public static var debug = new LogType("[DEBUG] ");
+	static final prefixes = {
+		fatal: "[FATAL] ",
+		error: "[ERROR] ",
+		warn: "[WARN]  ",
+		info: "[INFO]  ",
+		debug: "[DEBUG] "
+	};
+
+	public static var fatal = new LogType(prefixes.fatal);
+	public static var error = new LogType(prefixes.error);
+	public static var warn = new LogType(prefixes.warn);
+	public static var info = new LogType(prefixes.info);
+	public static var debug = new LogType(prefixes.debug);
 
 	/**
 		Recreate all types in `LogTypes`.
@@ -16,10 +24,10 @@ class LogTypes {
 		This can be used e.g. for reflecting changes of static variables in `LogType`.
 	**/
 	public static function reset() {
-		fatal = new LogType("[FATAL] ");
-		error = new LogType("[ERROR] ");
-		warn = new LogType("[WARN]  ");
-		info = new LogType("[INFO]  ");
-		debug = new LogType("[DEBUG] ");
+		fatal = new LogType(prefixes.fatal);
+		error = new LogType(prefixes.error);
+		warn = new LogType(prefixes.warn);
+		info = new LogType(prefixes.info);
+		debug = new LogType(prefixes.debug);
 	}
 }
