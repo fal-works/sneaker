6cddd77174a804238ed611ca2c5823c0271f935f
FAL <falworks.contact@gmail.com>
2020-03-09 05:52:06 +0900

    log: MacroLogger: add completion display functions

--- src/sneaker/log/MacroLogger.hx
+++ src/sneaker/log/MacroLogger.hx
@@ -47,5 +47,29 @@ class MacroLogger {
 
 		return buffer;
 	}
+
+	/**
+		Displays a compilation fatal error at the position where macro was called.
+	**/
+	public static inline function fatal(message: String): Void
+		Context.fatalError(message, Context.currentPos());
+
+	/**
+		Displays a compilation error at the position where macro was called.
+	**/
+	public static inline function error(message: String): Void
+		Context.error(message, Context.currentPos());
+
+	/**
+		Displays a compilation warning at the position where macro was called.
+	**/
+	public static inline function warn(message: String): Void
+		Context.warning(message, Context.currentPos());
+
+	/**
+		Displays a compilation info at the position where macro was called.
+	**/
+	public static inline function info(message: String): Void
+		Context.info(message, Context.currentPos());
 }
 #end
