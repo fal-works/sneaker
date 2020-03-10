a85a57dba43db535423852887a438c6b60983f05
FAL <falworks.contact@gmail.com>
2020-03-10 21:56:50 +0900

    format: StringExtension: add indexOf functions

--- src/sneaker/format/StringExtension.hx
+++ src/sneaker/format/StringExtension.hx
@@ -15,30 +15,50 @@ class StringExtension {
 	}
 
 	/**
+		@return The first index of dot (`.`).
+	**/
+	public static inline function indexOfDot(s: String, ?startIndex: Int): Int
+		return s.indexOf(dot, startIndex);
+
+	/**
+		@return The first index of slash (`/`).
+	**/
+	public static inline function indexOfSlash(s: String, ?startIndex: Int): Int
+		return s.indexOf(slash, startIndex);
+
+	/**
+		@return The last index of dot (`.`).
+	**/
+	public static inline function lastIndexOfDot(s: String, ?startIndex: Int): Int
+		return s.lastIndexOf(dot, startIndex);
+
+	/**
+		@return The last index of slash (`/`).
+	**/
+	public static inline function lastIndexOfSlash(s: String, ?startIndex: Int): Int
+		return s.lastIndexOf(slash, startIndex);
+
+	/**
 		@return Sub-string after the last occurrence of dot (`.`).
 	**/
-	public static inline function sliceAfterLastDot(s: String): String {
-		return s.substr(s.lastIndexOf(dot) + 1);
-	}
+	public static inline function sliceAfterLastDot(s: String): String
+		return s.substr(lastIndexOfDot(s) + 1);
 
 	/**
 		@return Sub-string after the last occurrence of slash (`/`).
 	**/
-	public static inline function sliceAfterLastSlash(s: String): String {
-		return s.substr(s.lastIndexOf(slash) + 1);
-	}
+	public static inline function sliceAfterLastSlash(s: String): String
+		return s.substr(lastIndexOfSlash(s) + 1);
 
 	/**
 		@return Sub-string before the last occurrence of dot (`.`).
 	**/
-	public static inline function sliceBeforeLastDot(s: String): String {
-		return s.substr(0, s.lastIndexOf(dot));
-	}
+	public static inline function sliceBeforeLastDot(s: String): String
+		return s.substr(0, lastIndexOfDot(s));
 
 	/**
 		@return Sub-string before the last occurrence of slash (`/`).
 	**/
-	public static inline function sliceBeforeLastSlash(s: String): String {
-		return s.substr(0, s.lastIndexOf(slash));
-	}
+	public static inline function sliceBeforeLastSlash(s: String): String
+		return s.substr(0, lastIndexOfSlash(s));
 }
