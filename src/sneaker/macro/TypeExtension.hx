b3cdaeef458877ffe2aaf9c1b0d834cdc1ff9d76
FAL <falworks.contact@gmail.com>
2020-03-11 01:17:44 +0900

    macro: add TypeExtension class

---
+++ src/sneaker/macro/TypeExtension.hx
@@ -0,0 +1,14 @@
+package sneaker.macro;
+
+import haxe.macro.Type;
+
+class TypeExtension {
+	public static function isClassWithName(type: Type, name: String): Bool {
+		return switch type {
+			case TInst(typeRef, params):
+				typeRef.toString() == name;
+			default:
+				false;
+		}
+	}
+}
