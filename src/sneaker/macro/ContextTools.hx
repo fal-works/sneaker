4637c68284699b2613cf852e320d70d00cb156bc
FAL <falworks.contact@gmail.com>
2020-03-11 01:32:10 +0900

    macro: add ContextTools functions

--- src/sneaker/macro/ContextTools.hx
+++ src/sneaker/macro/ContextTools.hx
@@ -1,6 +1,9 @@
 package sneaker.macro;
 
 #if macro
+import sneaker.macro.Types.MacroType;
+import sneaker.macro.Types.MacroModule;
+
 class ContextTools {
 	/**
 		@return Current file path, e.g. "src/Main.hx".
@@ -8,6 +11,32 @@ class ContextTools {
 	public static inline function getFilePath(): String {
 		final position = Context.getPosInfos(Context.currentPos());
 		return '${position.file}';
+	}
+
+	/**
+		@return `haxe.macro.Type` instance.
+		If not found, `null` instead of throwing exception.
+	**/
+	public static function tryGetType(typePath: String): Null<MacroType> {
+		try {
+			return Context.getType(typePath);
+		}
+		catch(e: Dynamic) {
+			return null;
+		}
+	}
+
+	/**
+		@return Array of all contained types.
+		If module not found, `null` instead of throwing exception.
+	**/
+	public static function tryGetModule(modulePath: String): Null<MacroModule> {
+		try {
+			return Context.getModule(modulePath);
+		}
+		catch(e: Dynamic) {
+			return null;
+		}
 	}
 }
 #end
