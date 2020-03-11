93a396c86c56f8e6a0eb7d2ddeea1581f82c4de4
FAL <falworks.contact@gmail.com>
2020-03-12 00:31:06 +0900

    print: fix compile error if sneaker_print_disable

--- src/sneaker/print/PrinterCallbacks.hx
+++ src/sneaker/print/PrinterCallbacks.hx
@@ -2,25 +2,52 @@ package sneaker.print;
 
 class PrinterCallbacks {
 	/** @see `Printer.println()` **/
-	public static final println = (s: String) -> Printer.println(s);
+	public static final println = (s: String) -> {
+		#if !sneaker_print_disable
+		Printer.println(s);
+		#end
+	}
 
 	/** @see `Printer.print()` **/
-	public static final print = (s: String) -> Printer.print(s);
+	public static final print = (s: String) -> {
+		#if !sneaker_print_disable
+		Printer.print(s);
+		#end
+	}
 
 	/** @see `Printer.printlnDirect()` **/
-	public static final printlnDirect = (s: String) -> Printer.printlnDirect(s);
+	public static final printlnDirect = (s: String) -> {
+		#if !sneaker_print_disable
+		Printer.printlnDirect(s);
+		#end
+	}
 
 	/** @see `Printer.printDirect()` **/
-	public static final printDirect = (s: String) -> Printer.printDirect(s);
+	public static final printDirect = (s: String) -> {
+		#if !sneaker_print_disable
+		Printer.printDirect(s);
+		#end
+	}
 
 	/** @see `Printer.printlnForced()` **/
-	public static final printlnForced = (s: String) -> Printer.printlnForced(s);
+	public static final printlnForced = (s: String) -> {
+		#if !sneaker_print_disable
+		Printer.printlnForced(s);
+		#end
+	}
 
 	/** @see `Printer.printForced()` **/
-	public static final printForced = (s: String) -> Printer.printForced(s);
+	public static final printForced = (s: String) -> {
+		#if !sneaker_print_disable
+		Printer.printForced(s);
+		#end
+	}
 
 	/** @see `Printer.printlnReturn()` **/
-	public static final printlnReturn = (s: String) -> Printer.printlnReturn(s);
+	public static final printlnReturn = (s: String) -> {
+		Printer.printlnReturn(s);
+		return s;
+	}
 
 	/** @see `Printer.printlnThrow()` **/
 	public static final printlnThrow = (s: String) -> Printer.printlnThrow(s);
