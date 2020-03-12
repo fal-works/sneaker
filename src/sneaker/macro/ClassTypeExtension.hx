78f244176819151a03ee2f4b1750fb1f7e09b72d
FAL <falworks.contact@gmail.com>
2020-03-12 20:01:00 +0900

    macro: add utility static extension classes

---
+++ src/sneaker/macro/ClassTypeExtension.hx
@@ -0,0 +1,24 @@
+package sneaker.macro;
+
+#if macro
+import haxe.macro.Type.ClassType;
+
+class ClassTypeExtension {
+	/**
+		Recursively checks if any of super-classes of `this` has metadata with `metadataName`.
+		@return `true` if any super-class has the metadata.
+	**/
+	public static function anySuperClassHasMetadata(
+		_this: ClassType,
+		metadataName: String
+	): Bool {
+		final superClass = _this.superClass;
+		if (superClass == null) return false;
+
+		final superClassType = superClass.t.get();
+		if (superClassType.meta.has(metadataName)) return true;
+
+		return anySuperClassHasMetadata(superClassType, metadataName);
+	}
+}
+#end
