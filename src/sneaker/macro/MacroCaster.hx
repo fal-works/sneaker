424937305d07b860723857cdd0e2cbf56610eaac
FAL <falworks.contact@gmail.com>
2020-03-16 21:12:56 +0900

    macro: add MacroCaster class

---
+++ src/sneaker/macro/MacroCaster.hx
@@ -0,0 +1,31 @@
+package sneaker.macro;
+
+#if macro
+import haxe.macro.Expr;
+import haxe.macro.Type;
+
+/**
+	Functions for converting macro-related types.
+**/
+class MacroCaster {
+	/**
+		@param param `Type.TypeParameter`
+		@return Enum `Expr.TypeParam` (instance `TPType`)
+	**/
+	public static function typeParameterToTypeParam(param: TypeParameter): TypeParam
+		return TPType(Context.toComplexType(param.t));
+
+	/**
+		@param `Type.Ref<ClassType>`
+		@return `Expr.TypePath`
+	**/
+	public static function typeRefToTypePath(typeRef: Ref<ClassType>): TypePath {
+		final type = typeRef.get();
+		return {
+			pack: type.pack,
+			name: type.name,
+			params: type.params.map(typeParameterToTypeParam)
+		}
+	}
+}
+#end
