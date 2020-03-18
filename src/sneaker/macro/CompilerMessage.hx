e07b8c1869d9dbaf8fbd1f259cf797bb4c51484b
FAL <falworks.contact@gmail.com>
2020-03-19 07:19:45 +0900

    macro: use PositionStack in CompilerMessage

--- src/sneaker/macro/CompilerMessage.hx
+++ src/sneaker/macro/CompilerMessage.hx
@@ -11,35 +11,35 @@ package sneaker.macro;
 **/
 class CompilerMessage {
 	/**
-		Displays a compilation fatal error at the position where macro was called.
+		Displays a compilation fatal error at the position most recently saved in `PositionStack`.
 	**/
 	public static inline function fatal(message: String): Void {
 		if (CompilerFlags.macroMessageLevel.get() >= 100)
-			Context.fatalError(message, Context.currentPos());
+			Context.fatalError(message, PositionStack.peek());
 	}
 
 	/**
-		Displays a compilation error at the position where macro was called.
+		Displays a compilation error at the position most recently saved in `PositionStack`.
 	**/
 	public static inline function error(message: String): Void {
 		if (CompilerFlags.macroMessageLevel.get() >= 200)
-			Context.error(message, Context.currentPos());
+			Context.error(message, PositionStack.peek());
 	}
 
 	/**
-		Displays a compilation warning at the position where macro was called.
+		Displays a compilation warning at the position most recently saved in `PositionStack`.
 	**/
 	public static inline function warn(message: String): Void {
 		if (CompilerFlags.macroMessageLevel.get() >= 300)
-			Context.warning(message, Context.currentPos());
+			Context.warning(message, PositionStack.peek());
 	}
 
 	/**
-		Displays a compilation info at the position where macro was called.
+		Displays a compilation info at the position most recently saved in `PositionStack`.
 	**/
 	public static inline function info(message: String): Void {
 		if (CompilerFlags.macroMessageLevel.get() >= 400)
-			Context.info(message, Context.currentPos());
+			Context.info(message, PositionStack.peek());
 	}
 }
 #end
