2f8f22d841909dc8948fb952668afb3d0565a493
FAL <falworks.contact@gmail.com>
2020-03-20 07:33:13 +0900

    macro: add ComplexTypeExtension

---
+++ src/sneaker/macro/ComplexTypeExtension.hx
@@ -0,0 +1,18 @@
+package sneaker.macro;
+
+#if macro
+import haxe.macro.Expr;
+import sneaker.types.Result;
+
+class ComplexTypeExtension {
+	/**
+		@return Return type of `complexType` if it is a function. otherwise `Failed`.
+	**/
+	public static function getReturnType(complexType: ComplexType): Result<ComplexType, String> {
+		return switch complexType {
+			case TFunction(_, returnType): Ok(returnType);
+			default: Failed('Not a function');
+		}
+	}
+}
+#end
