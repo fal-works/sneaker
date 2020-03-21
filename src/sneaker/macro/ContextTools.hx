74dcf18314f2ad93038e312ba116170f0cb2a061
FAL <falworks.contact@gmail.com>
2020-03-21 12:34:15 +0900

    macro: add ContextTools.getLocalClass()

--- src/sneaker/macro/ContextTools.hx
+++ src/sneaker/macro/ContextTools.hx
@@ -3,8 +3,25 @@ package sneaker.macro;
 #if macro
 import sneaker.macro.Types.MacroType;
 import sneaker.macro.Types.MacroModule;
+import haxe.macro.Type.ClassType;
 
 class ContextTools {
+	/**
+		`MacroResult` version of `Context.getLocalClass()`.
+		@return The current class in which the macro was called.
+	**/
+	public static function getLocalClass(): MacroResult<ClassType> {
+		final localClassRef = Context.getLocalClass();
+
+		return if (localClassRef != null)
+			Ok(localClassRef.get());
+		else
+			Failed(
+				'Failed to get local class',
+				Context.currentPos()
+			);
+	}
+
 	/**
 		@return Current file path, e.g. "src/Main.hx".
 	**/
