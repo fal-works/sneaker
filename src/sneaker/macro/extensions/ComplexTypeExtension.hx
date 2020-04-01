256710df3f08497424567b30cd87c51d92a2c02e
FAL <falworks.contact@gmail.com>
2020-04-01 13:11:29 +0900

    macro: add ComplexType.unwrapNamed()

--- src/sneaker/macro/extensions/ComplexTypeExtension.hx
+++ src/sneaker/macro/extensions/ComplexTypeExtension.hx
@@ -1,12 +1,14 @@
 package sneaker.macro.extensions;
 
 #if macro
+using sneaker.macro.TypeMapper;
+
 import haxe.macro.Expr;
 import sneaker.types.Result;
 
 class ComplexTypeExtension {
 	/**
-		@return Return type of `complexType` if it is a function. otherwise `Failed`.
+		@return The return type of `complexType` if it is a function. otherwise `Failed`.
 	**/
 	public static function getReturnType(
 		complexType: ComplexType
@@ -14,6 +16,22 @@ class ComplexTypeExtension {
 		return switch complexType {
 			case TFunction(_, returnType): Ok(returnType);
 			default: Failed('Not a function');
+		}
+	}
+
+	/**
+		Reveals the underlying type if `TNamed` (recursively).
+	**/
+	public static function unwrapNamed(complexType: ComplexType): ComplexType
+		return complexType.mapTypes(unwrapNamedShallow);
+
+	/**
+		Reveals the underlying type if `TNamed` (not recursively).
+	**/
+	public static function unwrapNamedShallow(complexType: ComplexType): ComplexType {
+		return switch complexType {
+			case TNamed(_, underlyingType): underlyingType;
+			default: complexType;
 		}
 	}
 }
