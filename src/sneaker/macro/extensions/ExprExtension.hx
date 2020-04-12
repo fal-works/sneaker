076808b2f03125e6ae2ea7db5c0e4736a7d3d830
FAL <falworks.contact@gmail.com>
2020-04-12 15:38:39 +0900

    macro: add Expr.as()

--- src/sneaker/macro/extensions/ExprExtension.hx
+++ src/sneaker/macro/extensions/ExprExtension.hx
@@ -7,16 +7,22 @@ class ExprExtension {
 		@return `true` if the type of this expression unifies `complexType`.
 	**/
 	public static function unify(_this: Expr, complexType: ComplexType): Bool {
-		final check: Expr = {
-			expr: ECheckType(_this, complexType),
-			pos: _this.pos
-		};
 		try {
-			Context.typeof(check);
+			Context.typeof(as(_this, complexType));
 			return true;
 		} catch (e: Dynamic) {
 			return false;
 		}
+	}
+
+	/**
+		@return `(expr: T)` expression.
+	**/
+	public static function as(_this: Expr, complexType: ComplexType): Expr {
+		return {
+			expr: ECheckType(_this, complexType),
+			pos: _this.pos
+		};
 	}
 }
 #end
