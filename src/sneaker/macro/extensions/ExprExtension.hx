f39ad9eec140965646f7a04f4a20615d671dc473
FAL <falworks.contact@gmail.com>
2020-04-17 21:44:38 +0900

    macro: add Expr.getValue() etc.

--- src/sneaker/macro/extensions/ExprExtension.hx
+++ src/sneaker/macro/extensions/ExprExtension.hx
@@ -3,6 +3,8 @@ package sneaker.macro.extensions;
 #if macro
 import haxe.macro.Expr;
 import haxe.macro.Type;
+import haxe.macro.ExprTools;
+import sneaker.types.Maybe;
 
 class ExprExtension {
 	/**
@@ -52,6 +54,69 @@ class ExprExtension {
 		final expression = macro @:privateAccess $_this;
 		expression.pos = _this.pos;
 		return expression;
+	}
+
+	/**
+		@return Value that `this` represents.
+		`Maybe.none()` if `this` is not supported by `haxe.macro.ExprTools.getValue`.
+	**/
+	public static function getValue(_this: Expr): Maybe<Dynamic> {
+		try {
+			return Maybe.from(ExprTools.getValue(_this));
+		} catch (_: Dynamic) {
+			return Maybe.none();
+		}
+	}
+
+	/**
+		@return `Int` literal value that `this` represents.
+	**/
+	public static function getIntLiteralValue(_this: Expr): Maybe<Int> {
+		return switch (_this.expr) {
+			case EConst(CInt(v)): Maybe.from(Std.parseInt(v));
+			default: Maybe.none();
+		}
+	}
+
+	/**
+		@return `Float` literal value that `this` represents.
+	**/
+	public static function getFloatLiteralValue(_this: Expr): Maybe<Float> {
+		return switch (_this.expr) {
+			case EConst(CFloat(v)): Maybe.from(Std.parseFloat(v));
+			default: Maybe.none();
+		}
+	}
+
+	/**
+		@return `String` literal value that `this` represents.
+	**/
+	public static function getStringLiteralValue(_this: Expr): Maybe<String> {
+		return switch (_this.expr) {
+			case EConst(CString(s)): s;
+			default: Maybe.none();
+		}
+	}
+
+	/**
+		@return `Bool` identifier value that `this` represents.
+	**/
+	public static function getBoolIdentifierValue(_this: Expr): Maybe<Bool> {
+		return switch (_this.expr) {
+			case EConst(CIdent("true")): Maybe.from(true);
+			case EConst(CIdent("false")): Maybe.from(false);
+			default: Maybe.none();
+		}
+	}
+
+	/**
+		@return `true` if `this` is a `null` identifier.
+	**/
+	public static function isNullIdentifier(_this: Expr): Bool {
+		return switch (_this.expr) {
+			case EConst(CIdent("null")): true;
+			default: false;
+		}
 	}
 }
 #end
